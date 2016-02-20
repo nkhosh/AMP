09656ee7840a44a33e7c8e58fea032eae257af09
IRNiti <ioana.niti@mail.mcgill.ca>
2016-02-20 16:55:32 -0500

    working unit test

---
+++ app/src/test/java/sep/amp/AmpUnitTest.java
@@ -0,0 +1,16 @@
+package sep.amp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Raluca on 20/02/2016.
+ */
+public class AmpUnitTest {
+
+    @Test
+    public void minLength() throws Exception {
+        assertTrue(MainActivity.length > 140);
+    }
+}
